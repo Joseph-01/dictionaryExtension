4fcb13a40c5d56fe86fa08693711d7599435ea80
Joseph <softjoe40@gmail.com>
2022-03-13 00:05:37 +0100

    manifest now working

--- public/background.js
+++ public/background.js
@@ -1,5 +1,14 @@
-chrome.browserAction.onClicked.addListener(function () {
-    chrome.tabs.executeScript(null, {
-        code: "alert('hello');"
-    })
-})
+function onExecuted(result) {
+    console.log(`We made it green`);
+}
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+
+const makeItGreen = 'document.body.style.border = "5px solid green"';
+
+const executing = browser.tabs.executeScript({
+    code: makeItGreen
+});
+executing.then(onExecuted, onError);
--- public/script.js
+++ public/script.js
@@ -1,14 +1,34 @@
 function getSelectionText() {
     alert("I am working now")
+    // var selectedText = '';
+
+    var text = window.getSelection()
+    // if (window.getSelection) {
+    //     selectedText = window.getSelection();
+    // }
+    // // document.getSelection
+    // else if (document.getSelection) {
+    //     selectedText = document.getSelection();
+    // }
+    // // document.selection
+    // else if (document.selection) {
+    //     selectedText =
+    //         document.selection.createRange().text;
+    // } else return;
+    // // To write the selected text into the textarea
+    // document.getElementById("display").innerHTML = selectedText;
 }
 
 
 let clickBtn = document.getElementById("click")
 
 clickBtn.addEventListener("click", function () {
-    // alert("hello")
-        chrome.tabs.executeScript(null, {
-            code: getSelectionText()
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.scripting.executeScript({
+            target: {tabId: tabs[0].id},
+            function: getSelectionText
         })
+    })
+
 })
 
